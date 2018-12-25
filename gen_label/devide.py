242ce8e40a4ed1d333fdcabbfe58153f9a185794
Jasonzhu0314 <jasonzhu_0314@163.com>
2018-12-25 10:34:09 +0800

    new workspave

--- gen_label/devide.py
+++ gen_label/devide.py
@@ -1,19 +1,49 @@
+import sys
 from numpy import random
-def data_separate():
-      all_txt='/lfs1/users/szhu/project/MTCNN-keras/data/12/label.txt'
-      train_txt='/lfs1/users/szhu/project/MTCNN-keras/data/12/label-train.txt'
-      test_txt='/lfs1/users/szhu/project/MTCNN-keras/data/12/label-test.txt'
-      test = open(test_txt,'w')
-      train = open(train_txt,'w')
-      with open(all_txt,'r') as a:
+
+# devide the total data into train and val 
+# select the (-neg_val_num/3 - 0) as the val data  
+
+
+def data_separate(img_dir,path,size):
+      global neg_val_num
+      all_txt= img_dir + size +'/' + path + size + '.txt'
+      train_txt= img_dir + size +'/' + path + size + '_train.txt'
+      val_txt= img_dir + size +'/' + path + size + '_val.txt'
+      val = open(val_txt, 'w')
+      train = open(train_txt, 'w')
+      with open(all_txt, 'r') as a:
             lines = a.readlines()
             random.shuffle(lines)
-            for i in range(int(len(lines)*0.9)):
-                  train.write(lines[i])
-                  if i % 1000 == 0:
-                      print i
-            for i in range(int(len(lines)*0.9), int(len(lines))):
-                  test.write(lines[i])
-      test.close()
+            for i in range(int(len(lines) * 0.999)):
+                  p = lines[i].find(" ") + 1
+                  if path == "pos_" or path == "part_":
+                        string = img_dir + lines[i][:p-1] + ".jpg " + lines[i][p:-1] + "\n"
+                  else:
+                        string = img_dir + lines[i][:p-1] + ".jpg " + lines[i][p:-1] + " -1 -1 -1 -1\n"
+                  train.write(string)
+                  if i % 2000000 == 0:
+                      print(i)
+            if path == "neg_":
+                  neg_val_num = int(len(lines)*0.001)
+                  start = -neg_val_num
+            else:
+                  start = -int(neg_val_num/3)
+            for i in range(start,0):
+                  p = lines[i].find(" ") + 1
+                  if path == "pos_" or path == "part_":
+                        string = img_dir + lines[i][:p-1] + ".jpg " + lines[i][p:-1] + "\n"
+                  else:
+                        string = img_dir + lines[i][:p-1] + ".jpg " + lines[i][p:-1] + " -1 -1 -1 -1\n"
+                  val.write(string)
+      val.close()
       train.close()
-data_separate()
+
+size = sys.argv[1]
+img_dir = "/home/users/zhuzhengshuai/data/mtcnn/data/"
+
+
+
+txt_lists = ["neg_","pos_","part_"]
+for path in txt_lists:
+      data_separate(img_dir,path,size)
